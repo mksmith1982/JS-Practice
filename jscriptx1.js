df4ee9e339c2a9040ff7b08bb67c339694ae866a
mksmith1982 <mksmith1982@gmail.com>
2016-10-05 23:35:33 -0400

    initial commit

---
+++ jscriptx1.js
@@ -0,0 +1,117 @@
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+////    ______                _   _                ////
+////    |  ___|              | | (_)               ////
+////    | |_ _   _ _ __   ___| |_ _  ___  _ __     ////
+////    |  _| | | | '_ \ / __| __| |/ _ \| '_ \    ////
+////    | | | |_| | | | | (__| |_| | (_) | | | |   ////
+////    \_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|   ////
+////                                               ////
+////                                               ////
+////    ______               _   _                 ////
+////    | ___ \             | | (_)                ////
+////    | |_/ / __ __ _  ___| |_ _  ___ ___        ////
+////    |  __/ '__/ _` |/ __| __| |/ __/ _ \       ////
+////    | |  | | | (_| | (__| |_| | (_|  __/       ////
+////    \_|  |_|  \__,_|\___|\__|_|\___\___|       ////
+////                                               ////
+////                                               ////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+function sum( x, y ) {
+	return(x+y);
+}
+
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+var avg= function ( x,y,z ){
+var answer=(x+y+z) / 3
+	return answer;
+
+}
+
+
+
+
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+
+
+for var
+}
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+function greaterThan(x,y) {
+	if (y>x) { 
+		return "true";
+	} 
+	else { 
+		return "false";
+	}
+
+}
+
+
+
+
+	
+}
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+var name="Matt"
+
+
+
+function greet(name) {
+	return "Hello, " + name + "!";
+	
+}
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function 
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+var a="quick";
+var b="fox";
+var c="jumps";
+var d="fence";
+
+function madlib (a,b,c,d) { 
+	return "the "+ a + " brown " +b + c +" over the " +d;
+	
+}
+
+
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
---
+++ jscriptx2.js
@@ -0,0 +1,59 @@
+// ---------------------
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// ---------------------
+
+function max(1,2){
+	if (2>1) {return "Two is the largest number."};
+
+	if-then-else (2<1)	{return "This is incorrect."}
+
+    //...
+}
+
+// ---------------------
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// ---------------------
+
+function maxOfThree(4,6,8){ 
+	if (8>6) {return "8 is the largest"};
+	if (8>4) {return "8 is the largest"};
+	else {return "false"};
+    //...
+}
+
+// ---------------------
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// ---------------------
+var char=("b")
+var vowel=["a","e","i","o","u"]
+var consonant=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+function isVowel(char , vowel , consonant)
+{
+	if (char === vowel ) {return "true"};
+	if (char === consonant) {return "false"};
+	
+    
+}
+
+// ---------------------
+// Write a function translate() that will translate a text into "Rovarspraket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+// ---------------------
+var consonant=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+var phrase= "rovarspraket"
+function rovarspraket(phrase , consonant){ 
+	for (var consonant = 0; i < Things.length; i++) {
+		Things[i]
+	}
+    //...
+}
+
+
+// ---------------------
+// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+// ---------------------
+var word="iron yard"
+function reverse(word){ return reverse
+	console.log
+	
+   
+};
